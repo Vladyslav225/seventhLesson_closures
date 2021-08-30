fa4fa8be1058c2f9f5a845aabd68b534010cdfa8
Yarynovskiy Vladyslav <yvv.work@gmail.com>
2021-08-30 16:34:29 +0300

    .

--- hw_functions.py
+++ hw_functions.py
@@ -5,46 +5,31 @@ import json
 
 response = requests.get('https://61261a39e40e190017072808.mockapi.io/test')
 
-
-
 list_obj = response.json()[0]
-# print(type(list_obj))
-# print(list_obj)
 
 
-def get_res(data):
-     
-     for obj in data:
+def get_res(search):
 
-          for key in obj.keys():
+     if type(search) == list:
 
-               value = obj[key]
-               
+          for obj in search:
+               get_res(obj)
+
+     elif type(search) == dict:
+
+          for key in search:
+
+               value = search[key]
+
                if type(value) == list:
                     get_res(value)
 
-               if type(value) == dict:
-                    get_value = value.get('res')
-                    print(get_value)
-                    
+               elif type(value) == dict :
+                    result = value.get('res')
+                    print(result)
 
-for key in list_obj.keys():
-     value = list_obj[key]
+                    if result == 'res':
+                         break
 
-     if type(value) == list:
-          get_res(list_obj[key])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+get_res(list_obj)
 
